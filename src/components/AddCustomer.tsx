936bdd37ebd7d927ee3535e5fcc0c3b14b1b5bed
ikonenjoel <drakening@gmail.com>
2026-05-07 11:06:51 +0300

    Basic functionality for adding customers, still missing POST method and UI elements that are added to CustomerList.tsx later.

---
+++ src/components/AddCustomer.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+import { type Customer } from "./CustomerList";
+
+interface AddCustomerProps {
+  saveCustomer: (customer: Customer) => void;
+}
+
+export default function AddCustomer({ saveCustomer }: AddCustomerProps) {
+  const [open, setOpen] = useState(false);
+  const [customer, setCustomer] = useState<Customer>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    city: "",
+  });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const addCustomer = () => {
+    saveCustomer(customer);
+    handleClose();
+    // Tyhjentää lomakkeen lisäyksen jälkeen
+    setCustomer({
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      city: "",
+    });
+  };
+
+  return (
+    <div style={{ margin: "10px" }}>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Uusi asiakas
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Uusi asiakas</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Etunimi"
+            fullWidth
+            value={customer.firstname}
+            onChange={(e) =>
+              setCustomer({ ...customer, firstname: e.target.value })
+            }
+          />
+          <TextField
+            margin="dense"
+            label="Sukunimi"
+            fullWidth
+            value={customer.lastname}
+            onChange={(e) =>
+              setCustomer({ ...customer, lastname: e.target.value })
+            }
+          />
+          <TextField
+            margin="dense"
+            label="Sähköposti"
+            fullWidth
+            value={customer.email}
+            onChange={(e) =>
+              setCustomer({ ...customer, email: e.target.value })
+            }
+          />
+          <TextField
+            margin="dense"
+            label="Puhelin"
+            fullWidth
+            value={customer.phone}
+            onChange={(e) =>
+              setCustomer({ ...customer, phone: e.target.value })
+            }
+          />
+          <TextField
+            margin="dense"
+            label="Kaupunki"
+            fullWidth
+            value={customer.city}
+            onChange={(e) => setCustomer({ ...customer, city: e.target.value })}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Peruuta</Button>
+          <Button onClick={addCustomer}>Tallenna</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
